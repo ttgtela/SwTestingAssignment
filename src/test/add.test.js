a4f35ad107f24dbee03a9ee79d2efa5c0f38c636
Tenho Laakkio <tenho.laakkio@tuni.fi>
2024-12-03 17:18:46 +0200

    Updated add.test.js

--- src/test/add.test.js
+++ src/test/add.test.js
@@ -14,8 +14,20 @@ describe("add.js", () => {
     });
 
     describe("negative tests", function(){
+        it('should return 0 when no arguments are provided', () => {
+            expect(add()).to.equal(0);
+        });
         it("Adding two arrays, expecting Nan.", () =>{
             expect(add(['3', '2'],['2'])).to.deep.equal(NaN)
+        });
+        it('should handle `null` as 0', () => {
+            expect(add(4, null)).to.equal(4);
+            expect(add(null, null)).to.equal(0);
+        });
+        it('should return NaN when one or both arguments are non-numeric', () => {
+            expect(add('a', 4)).to.be.NaN;
+            expect(add(4, 'b')).to.be.NaN;
+            expect(add('a', 'b')).to.be.NaN;
         });
     });
 });
